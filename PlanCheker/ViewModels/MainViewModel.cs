9814ea2566c8560da59c861655dd80235b53e0e7
annieyhtipton@hotmail.com <annieyhtipton@hotmail.com>
2019-02-13 13:55:22 -0800

    Added CT Date checker

--- PlanCheker/ViewModels/MainViewModel.cs
+++ PlanCheker/ViewModels/MainViewModel.cs
@@ -33,7 +33,7 @@ namespace PlanCheker.ViewModels
                 new PlanConstraint(ConstraintBuilder.Build("Rectum", "V75Gy[%] <= 15")),
                 new PlanConstraint(ConstraintBuilder.Build("Rectum", "V65Gy[%] <= 35")),
                 new PlanConstraint(ConstraintBuilder.Build("Bladder", "V80Gy[%] <= 15")),
-
+                new PlanConstraint(new CTDateConstraint())
            });
         }
 
